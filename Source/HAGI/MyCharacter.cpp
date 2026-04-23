29654cbdd631cd15a67269a818596df1d6791a1b
oliverslott <oliverslottsony@gmail.com>
2026-04-23 20:52:44 +0200

    c++ blueprint

---
+++ Source/HAGI/MyCharacter.cpp
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyCharacter.h"
+
+// Sets default values
+AMyCharacter::AMyCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	AimPitch = CalculateAimPitch();
+}
+
+// Called to bind functionality to input
+void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+float AMyCharacter::CalculateAimPitch()
+{
+	if (!Controller)
+	{
+		return 0.0f;
+	}
+
+	float pitch = FRotator::NormalizeAxis(Controller->GetControlRotation().Pitch);
+
+	float NormalizedPitch = pitch / 90.0f;
+
+	return FMath::Clamp(NormalizedPitch, -1.0f, 1.0f);
+}
+
